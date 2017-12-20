dababbc34890ec2effeac1b9e0ca672b1a4ac4e5
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-19 21:20:03 -0600

    alpha: add getSizes function

--- javascript/alpha/alpha-nn.js
+++ javascript/alpha/alpha-nn.js
@@ -2,4 +2,8 @@
 
 var Neural = function(Neural) {
     'use strict'; //allows function in strict operating context - prevents certain/unexpected actions from running - safety check
+
+    function getSizes(nodes) { //feedforward NN - 128 hidden nodes - mcb180x(check it out)
+
+    }
 };
