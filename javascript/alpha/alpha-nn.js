4645acac27d22809328862a0f6296c4699c5bfab
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-19 21:17:50 -0600

    alpha: add nn

---
+++ javascript/alpha/alpha-nn.js
@@ -0,0 +1,5 @@
+//neural network
+
+var Neural = function(Neural) {
+    'use strict'; //allows function in strict operating context - prevents certain/unexpected actions from running - safety check
+};
